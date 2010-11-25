3705cc27bbb4d51165d6d4e473081c27efa57a9c
0x39dc <0x39dc@localhost>
2010-11-25 18:12:34 +0400

    Created project version 1.0

---
+++ SystemTimeFreezer/MainForm.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace SystemTimeFreezer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.dtFreeze = new System.Windows.Forms.DateTimePicker();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dtFreeze
+            // 
+            this.dtFreeze.CustomFormat = "dd/MM/yyyy HH:mm:ss";
+            this.dtFreeze.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtFreeze.Location = new System.Drawing.Point(22, 12);
+            this.dtFreeze.Name = "dtFreeze";
+            this.dtFreeze.Size = new System.Drawing.Size(150, 20);
+            this.dtFreeze.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(22, 38);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "&Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(97, 38);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "S&top";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Time Freezer";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 70);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.showToolStripMenuItem.Text = "&Show/Hide";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.btnStart;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(195, 68);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.dtFreeze);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Time Freezer";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtFreeze;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ SystemTimeFreezer/MainForm.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace SystemTimeFreezer
+{
+    public partial class MainForm : Form
+    {
+
+        [DllImport("kernel32.dll", EntryPoint = "GetSystemTime", SetLastError = true)]
+        public extern static void Win32GetSystemTime(ref SystemTime sysTime);
+
+        [DllImport("kernel32.dll", EntryPoint = "SetSystemTime", SetLastError = true)]
+        public extern static bool Win32SetSystemTime(ref SystemTime sysTime);
+
+        DateTime dt;
+
+        public event EventHandler Minimize;
+
+        public struct SystemTime
+        {
+            public ushort Year;
+            public ushort Month;
+            public ushort DayOfWeek;
+            public ushort Day;
+            public ushort Hour;
+            public ushort Minute;
+            public ushort Second;
+            public ushort Millisecond;
+        };
+
+        SystemTime updatedTime;
+
+        Timer t = new Timer();
+
+        public MainForm()
+        {
+            // Check program instances
+            string proc = Process.GetCurrentProcess().ProcessName;
+            Process[] processes = Process.GetProcessesByName(proc);
+            if (processes.Length > 1)
+            {
+                MessageBox.Show("Application is already running");
+                return;
+            }
+
+            InitializeComponent();
+
+            // Minimize event handler
+            this.Minimize += new EventHandler(this.Form_Minimize);
+            t.Interval = 2000;
+            t.Tick += (s, e) => Win32SetSystemTime(ref updatedTime);
+
+        }
+
+        protected override void WndProc(ref Message msg)
+        {
+            const int WM_SIZE = 0x0005;
+            const int SIZE_MINIMIZED = 1;
+
+            if ((msg.Msg == WM_SIZE) && ((int)msg.WParam == SIZE_MINIMIZED) &&
+            (this.Minimize != null))
+            {
+                this.Minimize(this, EventArgs.Empty);
+            }
+
+            base.WndProc(ref msg);
+        }
+
+        private void Form_Minimize(object sender, EventArgs e)
+        {
+            ShowTimer();
+        }
+
+        private SystemTime SetSystemTimeStatic(DateTime args)
+        {
+            dt = args;
+
+            SystemTime updatedTime = new SystemTime();
+            updatedTime.Year = (ushort)dt.Year;
+            updatedTime.Month = (ushort)dt.Month;
+            updatedTime.Day = (ushort)dt.Day;
+            // UTC time; it will be modified according to the regional settings of the target computer so the actual hour might differ
+            updatedTime.Hour = (ushort)dt.Hour;
+            updatedTime.Minute = (ushort)dt.Minute;
+            updatedTime.Second = (ushort)dt.Second;
+            return updatedTime;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            updatedTime = SetSystemTimeStatic(dtFreeze.Value.ToUniversalTime());
+
+            t.Start();
+            btnStop.Enabled = true;
+            EnableButtons();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            t.Stop();
+            btnStop.Enabled = false;
+            EnableButtons();
+        }
+
+        private void EnableButtons()
+        {
+            btnStart.Enabled = !btnStop.Enabled;
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ShowTimer();
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            ShowTimer();
+        }
+
+        private void ShowTimer()
+        {
+            this.Visible = !this.Visible;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
